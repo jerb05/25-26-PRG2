51eedca3e9ad30cebaecf1ff84e630b7484e6aa6
jerb05 <jerb0508@gmail.com>
2026-02-10 13:59:00 +0100

    cicloMenu Hace print al menu y se hace el ciclo para los comandos y lineas para que hagan lo que cada comando indica en el menu

--- evaluaciones/retos/reto002/entregas/riveraJorge/src/reto002/EditorDeTexto.java
+++ evaluaciones/retos/reto002/entregas/riveraJorge/src/reto002/EditorDeTexto.java
@@ -27,11 +27,36 @@ class EditorDeTexto {
                 System.out.println(i + ":" + marca + "| " + lineas[i]);
 
             }
+            System.out.println("Comandos: [L]inea | [E]ditar | [I]ntercambiar | [B]orrar | [S]alir: ");
+            String opcion = scanner.nextLine().toUpperCase();
+
+            if (opcion.equals("L")) {
+
+                System.out.print("Numero de linea (0-9): ");
+                lineaActiva = Integer.parseInt(scanner.nextLine());
+            }
+            else if (opcion.equals("B")) {
+                lineas[lineaActiva] = "";
+            }
+            else if (opcion.equals("I")) {
+                System.out.print("Intercambiar con linea numero: ");
+                int destino = Integer.parseInt(scanner.nextLine());
+                String temporal = lineas[lineaActiva];
+                lineas[lineaActiva] = lineas[destino];
+                lineas[destino] = temporal;
+            }
+            else if (opcion.equals("S")) {
+                ejecutar = false;
+                System.out.println("Programa cerrado.");
+            }
+        }
+        scanner.close();
+
 
         }
 
     }
 
-}
+
     
 
