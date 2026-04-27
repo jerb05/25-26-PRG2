17653021a9a0f2c04be629af8970ad585c6a0a02
jerb05 <jerb0508@gmail.com>
2026-04-27 22:38:14 +0200

    implementacion de clase Carrera y su estructura inicial

---
+++ evaluaciones/retos/reto005/Carrera.java
@@ -0,0 +1,11 @@
+class Carrera {
+
+    private Caballo c1;
+    private Caballo c2;
+    private final int META = 40;
+
+    public Carrera() {
+        this.c1 = new Caballo(1);
+        this.c2 = new Caballo(2);
+    }
+}
