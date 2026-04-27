ab557ca6175a15fa4b482b750422201b14763ced
jerb05 <jerb0508@gmail.com>
2026-04-27 23:08:58 +0200

    feat: visualizacion de la pista

--- evaluaciones/retos/reto005/Carrera.java
+++ evaluaciones/retos/reto005/Carrera.java
@@ -8,4 +8,17 @@ class Carrera {
         this.c1 = new Caballo(1);
         this.c2 = new Caballo(2);
     }
+
+    private void mostrarPista() {
+        System.out.println("----------------------------------------------");
+        dibujarCarril(c1.getPosicion());
+        dibujarCarril(c2.getPosicion());
+        System.out.println("----------------------------------------------\n")
+    }
+
+    private void dibujarCarril(int posicion) {
+    for (int i = 0; i < posicion; i++) {
+        System.out.print(" ");
+    }
+    System.out.println(";-;'");
 }
