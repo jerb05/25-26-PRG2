77a70bf3f33af0ab745c534c946b495c1ba5a5b9
jerb05 <jerb0508@gmail.com>
2026-05-11 21:28:05 +0200

    implementacion de clase Carta con los atirbutos palo y rango

---
+++ entregas/riveraJorge/src/Carta.java
@@ -0,0 +1,9 @@
+public class Carta {
+    private String palo;
+    private String rango;
+
+    public Carta(String palo, String rango) {
+        this.palo = palo;
+        this.rango = rango;
+    }
+}
